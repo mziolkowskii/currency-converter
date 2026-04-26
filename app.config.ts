323cd9060345bee15b45c02414571596c8e1e942
mziolkowskii <m.ziolkowski.runit@gmail.com>
2026-04-27 00:11:16 +0200

    feat: add custom app icon and splash screen

--- app.config.ts
+++ app.config.ts
@@ -7,32 +7,45 @@ const config: ExpoConfig = {
   slug: 'currency-converter',
   version: '1.0.0',
   orientation: 'portrait',
-  icon: './assets/images/icon.png',
   scheme: 'currencyconverter',
   userInterfaceStyle: 'automatic',
   platforms: ['ios', 'android'],
-  splash: {
-    image: './assets/images/splash-icon.png',
-    resizeMode: 'contain',
-    backgroundColor: '#FFFFFF',
-  },
   ios: {
     supportsTablet: false,
     bundleIdentifier: PACKAGE_NAME,
+    icon: {
+      dark: './assets/images/ios-dark.png',
+      light: './assets/images/ios-light.png',
+      tinted: './assets/images/ios-tinted.png',
+    },
   },
   android: {
     adaptiveIcon: {
       backgroundColor: '#FFFFFF',
-      foregroundImage: './assets/images/android-icon-foreground.png',
-      backgroundImage: './assets/images/android-icon-background.png',
-      monochromeImage: './assets/images/android-icon-monochrome.png',
+      foregroundImage: './assets/images/adaptive-icon.png',
+      monochromeImage: './assets/images/adaptive-icon.png',
     },
     package: PACKAGE_NAME,
     softwareKeyboardLayoutMode: 'pan',
     predictiveBackGestureEnabled: false,
   },
   owner: 'mziolkowskii',
-  plugins: ['expo-router'],
+  plugins: [
+    'expo-router',
+    [
+      'expo-splash-screen',
+      {
+        image: './assets/images/splash-icon-dark.png',
+        backgroundColor: '#FFFFFF',
+        imageWidth: 200,
+        resizeMode: 'contain',
+        dark: {
+          image: './assets/images/splash-icon-light.png',
+          backgroundColor: '#000000',
+        },
+      },
+    ],
+  ],
   experiments: {
     typedRoutes: true,
   },
