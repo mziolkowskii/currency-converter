da8cbe9e800ad3e9b0848129a928bb70982e1d5d
mziolkowskii <m.ziolkowski.runit@gmail.com>
2026-04-27 00:34:50 +0200

    fix: reduce useConvert stale time to 5 minutes

--- src/hooks/api/converter/useConvert.ts
+++ src/hooks/api/converter/useConvert.ts
@@ -1,7 +1,7 @@
 import { api } from '@services/api/api';
 import { CurrencyConversionRecord } from '@services/api/converter/types';
 import { useQuery } from '@tanstack/react-query';
-import { ONE_HOUR } from '@utils/common/timestamps';
+import { ONE_MINUTE } from '@utils/common/timestamps';
 
 type ConvertCurrencyProps = {
   from: string | null;
@@ -9,13 +9,13 @@ type ConvertCurrencyProps = {
   amount: number;
 };
 
-const CURRENCY_CONVERSION_QUERY_KEY = 'currency-coversion';
+const CURRENCY_CONVERSION_QUERY_KEY = 'currency-conversion';
 
 export const useConvert = ({ amount, from, to }: ConvertCurrencyProps) =>
   useQuery({
     queryKey: [CURRENCY_CONVERSION_QUERY_KEY, from, to, amount],
     queryFn: async () => api.get<CurrencyConversionRecord>('/convert', { params: { amount, from, to } }),
     select: data => data.data,
-    staleTime: ONE_HOUR,
+    staleTime: 5 * ONE_MINUTE,
     enabled: !!from && !!to && !!amount,
   });
